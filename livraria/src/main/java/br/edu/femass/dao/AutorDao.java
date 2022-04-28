09e621afc9a9c24e9625f54337cbaaf6bba1f5a7
Arthur Estephano Lemos de Moura <69534700+arthurEstephano@users.noreply.github.com>
2022-04-28 13:43:53 -0300

    Uploading livrary project on repo

---
+++ livraria/src/main/java/br/edu/femass/dao/AutorDao.java
@@ -0,0 +1,62 @@
+package br.edu.femass.dao;
+
+import br.edu.femass.model.Autor;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutorDao extends DaoPostgres implements  Dao<Autor>{
+    @Override
+    public List<Autor> listar() throws Exception {
+        String sql = "select * from autor order by nome";
+        PreparedStatement ps = getPreparedStatement(sql, false);
+        ResultSet rs = ps.executeQuery();
+
+        List<Autor> autores = new ArrayList<Autor>();
+        while (rs.next()) {
+            Autor autor = new Autor();
+            autor.setNome(rs.getString("nome"));
+            autor.setSobreNome(rs.getString("sobrenome"));
+            autor.setId(rs.getLong("id"));
+            autores.add(autor);
+        }
+
+        return autores;
+    }
+
+    @Override
+    public void gravar(Autor value) throws Exception {
+        String sql = "INSERT INTO autor (nome, sobrenome) VALUES (?,?)";
+        PreparedStatement ps = getPreparedStatement(sql, true);
+        ps.setString(1, value.getNome());
+        ps.setString(2, value.getSobreNome());
+
+        ps.executeUpdate();
+
+        ResultSet rs = ps.getGeneratedKeys();
+        rs.next();
+        value.setId(rs.getLong(1));
+
+    }
+
+    @Override
+    public void alterar(Autor value) throws Exception {
+        String sql = "update autor set nome = ?, sobrenome = ? where id = ?";
+        PreparedStatement ps = getPreparedStatement(sql, false);
+        ps.setString(1, value.getNome());
+        ps.setString(2, value.getSobreNome());
+        ps.setLong(3, value.getId());
+        ps.executeUpdate();
+
+    }
+
+    @Override
+    public void excluir(Autor value) throws Exception {
+        String sql = "delete from autor where id = ?";
+        PreparedStatement ps = getPreparedStatement(sql, false);
+        ps.setLong(1, value.getId());
+        ps.executeUpdate();
+    }
+}
